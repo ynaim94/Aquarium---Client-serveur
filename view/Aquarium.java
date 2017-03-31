281bc9578e95a4782d6b15641e49d9cf8fe4d003
ngrati <ngrati@b32f13f5-358b-4bf8-86d5-74f4d5d67875>
2017-03-31 08:00:07 +0000

    Modif de l'architechture Client

--- view/Aquarium.java
+++ view/Aquarium.java
@@ -3,12 +3,14 @@ package aqua;
 import java.io.*;
 import java.net.*;
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Aquarium {
 
     private AquaPanel contentPane;
-    static private PromptThread pt;
-
+    private AquaConnection aquaCon;
+    static int port;
+    static InetAddress address;
     private void displayGUI(String ImagesPath)
     {
 	
@@ -23,53 +25,63 @@ public class Aquarium {
 	// frame.setResizable(false);
     }
 
-    static private void createSocket(InetAddress address, int port)throws IOException
-    {
-	Socket socket = new Socket(address, port);
-	//	System.out.println("SOCKET = " + socket);
-	BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-	String str = "bonjour";
-	for (int i = 0; i < 10; i++) {
-	    	    out.println(str);
-	    str = in.readLine();
-	}
-	System.out.println("END");
-	out.println("END");
-	in.close();
-	out.close();
-	socket.close();
+    static private String promptIn()throws IOException
+    {    
+	/* create a scanner so we can read the command-line input*/
+	Scanner	scanner = new Scanner(System.in);
+
+	    /* prompt for the command*/
+	    System.out.print(">"); 
+	    /*get the input as a String*/
+	    String cmd = scanner.next(); // We can also use scanner.nextInt() to return an int if needed
+	    /*TODO send cmd to server*/
+	    /*TODO recieve respnse from server*/
+	cmd=cmd.intern();
+
+	return cmd;
+    }
+	static private void promptOut(String response)throws IOException
+    {	
+	System.out.print("<"+response); //+server's response
+
     }
 
     public static void main(String[] args) throws Exception
     {
+
 	/*Get Configuration data*/  
 	Config conf = new Config(args[0]);
-	final int port = conf.getTcpPort();
+	port = conf.getTcpPort();
 	final String ImagesPath = conf.getVisualRepertory();
-	final InetAddress address = conf.getIpAddress();
-	pt =new PromptThread();
-	//	if (cmd=="hello")//&&response =greeting 
-	/*Display the aquarium*/
-		SwingUtilities.invokeLater(new Runnable(){    
-			@Override
-			public void run() {
-			    new Aquarium().displayGUI(ImagesPath);
-			 
-			}
-		    });
-  
-	pt.start();
-   try{
-				createSocket(address, port);
-			    }
-			    catch(IOException e){
-				System.out.println("Sorry.. No connection found");	
-			    }
-			    	
+	address = conf.getIpAddress();
+
+	/*Prompt*/
+	System.out.print(">>>>>>>Enter your command please <<<<<<<\n");
+	String response ="NO RESPONSE YET\n\n",cmd="";
+	while(cmd==""){  
+	    cmd=promptIn();
+	    	if (cmd=="hello")//&&response =greeting 
+	    /*Display the aquarium*/
+	    SwingUtilities.invokeLater(new Runnable(){    
+		    @Override
+		    public void run() {
+			new Aquarium().displayGUI(ImagesPath);
+			
+		    }
+		});
+	    
+		promptOut(response);
+		/*   try{
 		
-		//    }
+	    }
+	    catch(IOException e){
+		System.out.println("Sorry.. IOError");	
+	    }	*/
+	    	System.out.println(String.format("your command is %s, your response is %s", cmd, response));
+	
+		cmd="";
+	}
     }
+    
+    
 }
-    
-    
