7c07a9fddf95710d37e93d5b162ba1457a5c339b
ynaim <ynaim@b32f13f5-358b-4bf8-86d5-74f4d5d67875>
2017-04-29 15:08:00 +0000

    Parser de commande fish + position ajouté

---
+++ view/view4/AquaConnection.java
@@ -0,0 +1,58 @@
+package aqua;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class AquaConnection extends Thread{
+    Scanner scanner;
+    /*cmd : store the command written in the prompt*/
+    /*response : to store the response  */
+    String cmd,response;
+    static InetAddress address;
+    static int port;
+
+    // try { 
+    static private Socket socket;
+    // } catch(IOException e){	System.out.println("Sorry..Error while creating Socket");}
+
+    private ArrayList<Fish> Fishes;
+    
+    ArrayList<Fish> getFishes(){
+	return Fishes;
+    }
+    public AquaConnection(InetAddress address, int port) throws IOException
+    {
+	address=address;
+	port=port;
+
+	cmd="";
+	response ="NO RESPONSE YET\n\n";
+	/* create a scanner so we can read the command-line input*/
+	scanner = new Scanner(System.in);
+	socket=new Socket(address,port);
+    }
+    
+    public void send(String cmd) throws IOException{
+	// DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+
+	// os.writeBytes(cmd + "\n");
+	PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
+	out.println(cmd);
+    }
+
+    public String receive() throws IOException{
+	BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+	return is.readLine();
+    }
+
+
+    public void  run(){
+	
+	
+    }       
+    
+
+
+}
---
+++ view/view4/Fish.java
@@ -0,0 +1,44 @@
+package aqua;
+import java.util.Arrays;
+import java.lang.*;
+import java.util.Objects;
+import java.util.ArrayList;
+
+public class Fish implements Cloneable{
+    String name;
+    ArrayList<Integer> initPosition;
+    int mobilityTime;
+    int dimensions[];
+    String imageName;
+
+    public Fish(String name, ArrayList<Integer> initPosition, int mobilityTime, int dimensions[], String imageName){
+	this.name=name;
+	this.initPosition=new ArrayList<>(initPosition);
+	this.dimensions=Arrays.copyOf(dimensions, 2);
+	this.mobilityTime=mobilityTime;
+	this.imageName=imageName;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+	return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object o){
+	if (o instanceof Fish) {
+	    Fish fish = (Fish) o;
+	    if (fish.name==this.name){
+		return true;
+	    }
+	}
+	return false;
+    }
+
+
+    @Override
+    public int hashCode(){
+	return Objects.hash(name);
+    }
+};
+    
