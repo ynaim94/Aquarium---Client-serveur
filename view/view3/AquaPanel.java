f696d53987533e432bbcd190dc24a8ad1b43d300
ngrati <ngrati@b32f13f5-358b-4bf8-86d5-74f4d5d67875>
2017-04-29 18:51:40 +0000

    Les poissons sont affichés depuis l'arrayList en taille proportionnelle demandée et en suivant les destinations s'ils existent

--- view/view3/AquaPanel.java
+++ view/view3/AquaPanel.java
@@ -1,4 +1,5 @@
 package aqua;
+import java.util.ArrayList;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -13,70 +14,105 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.util.*;
 
+
 class AquaPanel extends JPanel implements ActionListener{
 
-    private int x=0, y=0;   
+    private int x=0,y=0,z=0,w=0;
+    private static int height,width,scaleX,scaleY;
     private BufferedImage[]images = new BufferedImage[8];
-    ArrayList<Fish> Fishes;
+    private String[]fishImages=new String[8];
+    ArrayList<Fish> Fishes=new ArrayList<Fish>();;
+    Timer timer = new Timer(100, (ActionListener) this);
 
-  
-
-    void setFishes(ArrayList<Fish> Fishes){
-	this.Fishes=new ArrayList<Fish>(Fishes.size());
-	//	for (Fish item : Fishes){
-	//	    this.Fishes.add(item.clone());
-	//	}
+    void setFishes(ArrayList<Fish> newFishes){
+	/* initialisation ? */
+	/*/!\ traiter tous les cas avec les positions additionnelles */
+	for(Fish newItem : newFishes){
+	    if (this.Fishes.contains(newItem)){
+		Fish item=this.Fishes.get(this.Fishes.indexOf(newItem));
+		item.initPosition.addAll(newItem.initPosition);
+	    }
+	    else {
+		this.Fishes.add(newItem);
+	    }
+	}
     }
-    Timer timer = new Timer(60, (ActionListener) this);
+
+
     public AquaPanel(String name) {
-
-
-	
         timer.start();
         setOpaque(true);
         setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
-        setBackground(Color.WHITE);
-   
-        for(int i=0; i<8;i++){
-            String name1=name+String.format("/fish%d.png", i);
-        try {
-
-            images[i] = ImageIO.read(getClass().getResource(name1));
-
-        } catch(IOException ioe) {
-
-            System.out.println("Unable to fetch image.");
-            ioe.printStackTrace();
-
-        }
-       } 
+	/*an array with the names of the fish images*/
+	for(int k=0;k<8;k++)
+	    fishImages[k]= String.format("fish%d", k);
+	
+	for(int j=0;j<8;j++){
+	    /*the full path to charge an image*/
+	    String name1=name+String.format("/%s.png",fishImages[j]);
+	    try{
+		    images[j] = ImageIO.read(getClass().getResource(name1));		    
+	    }
+	    catch(IOException ioe) {
+		System.out.println("Unable to fetch image.");
+		ioe.printStackTrace();
+	    }
+	}	       
     }
-
-  
     
     @Override
-   public Dimension getPreferredSize() {
-         return (new Dimension(images[0].getWidth(), images[0].getHeight()));
+    public Dimension getPreferredSize(){
+	width=images[0].getWidth();
+	height= images[0].getHeight();
+	return (new Dimension(width,height));
     }
-
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+	/*display the the background*/
         g.drawImage(images[0], 0, 0, this);
-       
-        for(int i=1;i<8;i++){   
-        x=(int)(Math.random()*700);
-        y=(int)(Math.random()*500);
-        g.drawImage(images[i], x ,y, this);
-       
-      }
+	/*display the fishes*/
+	for(int i=0; i<Fishes.size();i++){
+	    /* match the fish name with the suitable image */
+	    for(int l=1;l<8;l++)
+		    if (fishImages[l].equals(Fishes.get(i).name)){ /*TODO: accept names with _ */
+			Fishes.get(i).imageIndex=l;
+			break;
+		    }
+		    else /*image par défaut*/
+			Fishes.get(i).imageIndex=2;
+	    //System.out.println("fishImages[imageIndex]="+fishImages[Fishes.get(i).imageIndex]+";Fishes.get("+i+").name="+Fishes.get(i).name+";" );
+	    /*Actual Position*/
+	    x=Fishes.get(i).initPosition.get(0);
+	    y=Fishes.get(i).initPosition.get(1);
+	    /*if the fish have an other position to move to*/
+	    if(Fishes.get(i).initPosition.size()>=4){
+		/*destination*/
+		z=Fishes.get(i).initPosition.get(2);
+		w=Fishes.get(i).initPosition.get(3);
+		/*Remove the privious position*/
+		Fishes.get(i).initPosition.remove(0);
+		Fishes.get(i).initPosition.remove(1);
+	    }
+	    // System.out.println("Size= "+Fishes.get(i).initPosition.size());
+	    /*manage the proportional size*/
+	    scaleX=width*Fishes.get(i).dimensions[0]/100;
+	    scaleY=height*Fishes.get(i).dimensions[1]/100;
+	    /*display the fish*/
+	    g.drawImage(images[Fishes.get(i).imageIndex],x,y,scaleX,scaleY,this);
+	    
+	}
+	
     }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-              x += 10;
-              y += 10;
-              repaint();
-        }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	/*x += 1;
+	  y += 1;*/
+	x=z;
+	y=w;
+	repaint();
     }
+}
 
--- view/view3/Fish.java
+++ view/view3/Fish.java
@@ -1,20 +1,43 @@
 package aqua;
 import java.util.Arrays;
+import java.lang.*;
+import java.util.Objects;
+import java.util.ArrayList;
 
-
-public class Fish {
+public class Fish implements Cloneable{
     String name;
-    int init_position[];
-    int mobility_time;
+    ArrayList<Integer> initPosition;
+    int mobilityTime;
     int dimensions[];
+   int imageIndex;
 
-    public Fish(String name, int init_position[], int mobility_time, int dimensions[]){
+    public Fish(String name, ArrayList<Integer> initPosition, int mobilityTime, int dimensions[]){
 	this.name=name;
-	this.init_position=Arrays.copyOf(init_position, 2);
+	this.initPosition=new ArrayList<>(initPosition);
 	this.dimensions=Arrays.copyOf(dimensions, 2);
-	this.mobility_time=mobility_time;
+	this.mobilityTime=mobilityTime;
     }
 
-    
-}
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+	return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object o){
+	if (o instanceof Fish) {
+	    Fish fish = (Fish) o;
+	    if (fish.name==this.name){
+		return true;
+	    }
+	}
+	return false;
+    }
+
+
+    @Override
+    public int hashCode(){
+	return Objects.hash(name);
+    }
+};
     
