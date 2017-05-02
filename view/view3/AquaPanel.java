caea5874d0c42c81c58ab45ed547fe5b3bda9548
ngrati <ngrati@b32f13f5-358b-4bf8-86d5-74f4d5d67875>
2017-05-02 16:07:09 +0000

    viw 3

--- view/view3/AquaPanel.java
+++ view/view3/AquaPanel.java
@@ -18,15 +18,20 @@ import java.util.*;
 class AquaPanel extends JPanel implements ActionListener{
 
     private int x=0,y=0,z=0,w=0;
-    private static int height,width,scaleX,scaleY;
+    private static int height,width,scaleX,scaleY;  
     private BufferedImage[]images = new BufferedImage[8];
-    private String[]fishImages=new String[8];
-    ArrayList<Fish> Fishes=new ArrayList<Fish>();;
-    Timer timer = new Timer(100, (ActionListener) this);
+    /*an array with the names of the fish images*/
+    private String[]fishImages={"background","sneakingFish","smilingFish","bubbleFish","oldFish","happyFish","madFish","lostFish"};
+    ArrayList<Fish> Fishes=new ArrayList<Fish>();
+    /*timer to repaint fishes in their new positions each 100 ms*/
+    private Timer timer = new Timer(100, (ActionListener) this);
+  
 
+        
     void setFishes(ArrayList<Fish> newFishes){
 	/* initialisation ? */
 	/*/!\ traiter tous les cas avec les positions additionnelles */
+	fishesToString(this.Fishes);
 	for(Fish newItem : newFishes){
 	    if (this.Fishes.contains(newItem)){
 		Fish item=this.Fishes.get(this.Fishes.indexOf(newItem));
@@ -37,21 +42,77 @@ class AquaPanel extends JPanel implements ActionListener{
 	    }
 	}
     }
+    
+    public void setStartFish(String cmd){
+	String tokens[] = cmd.split("[ ]+");
+	for(Fish fish : Fishes){
+	    if (fish.name.equals(tokens[1])){
+		fish.start();
+	    }
+	}
+    }
+
+    
+    public void setAddFish(String[] items){
+	ArrayList<Integer> initPosition = new ArrayList<Integer>();
+	String name = new String(items[1]);
+	int dimensions[] = new int[2];
+	int mobilityTime = 5; // par defaut
+	int started;
+	Fish tmpFish ;
+	initPosition.add(Integer.parseInt(items[3]));
+	initPosition.add(Integer.parseInt(items[4]));
+	dimensions[0] = Integer.parseInt(items[5]);
+	dimensions[1] = Integer.parseInt(items[6]);
+	started = 0;
+	tmpFish= new Fish(name, initPosition, mobilityTime, dimensions, started);
+	ArrayList<Fish> newFishes = new ArrayList<Fish>();
+	newFishes.add(tmpFish);
+	setFishes(newFishes);
+    }
+
+    public void setGetFishes(String[][] items){
+	ArrayList<Fish> newFishes = new ArrayList<Fish>();
+	int added = 0;
+	for (String[] s: items){
+	    ArrayList<Integer> initPosition = new ArrayList<Integer>();
+	    String name = new String(s[0]);
+	    int dimensions[] = new int[2];
+	    int mobilityTime; // par defaut
+	    Fish tmpFish ;
+	    int started = 1;
+	    initPosition.add(Integer.parseInt(s[2]));
+	    initPosition.add(Integer.parseInt(s[3]));
+	    dimensions[0] = Integer.parseInt(s[4]);
+	    dimensions[1] = Integer.parseInt(s[5]);
+	    mobilityTime = Integer.parseInt(s[6]);
+	    tmpFish= new Fish(name, initPosition, mobilityTime, dimensions,started);
+	    newFishes.add(tmpFish);
+	}
+
+	for(Fish fish : Fishes){
+	    if (!(newFishes.contains(fish))){
+		if (fish.started == 0){
+		    newFishes.add (fish);
+		}
+	    }
+	}
+	this.Fishes = newFishes;
+
+    }
+
+
 
 
     public AquaPanel(String name) {
         timer.start();
         setOpaque(true);
-        setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
-	/*an array with the names of the fish images*/
-	for(int k=0;k<8;k++)
-	    fishImages[k]= String.format("fish%d", k);
-	
+        setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));	
 	for(int j=0;j<8;j++){
 	    /*the full path to charge an image*/
-	    String name1=name+String.format("/%s.png",fishImages[j]);
+	    String fullPath=name+String.format("/%s.png",fishImages[j]);
 	    try{
-		    images[j] = ImageIO.read(getClass().getResource(name1));		    
+		images[j] = ImageIO.read(getClass().getResource(fullPath));		    
 	    }
 	    catch(IOException ioe) {
 		System.out.println("Unable to fetch image.");
@@ -60,11 +121,13 @@ class AquaPanel extends JPanel implements ActionListener{
 	}	       
     }
     
+    
+    
     @Override
     public Dimension getPreferredSize(){
-	width=images[0].getWidth();
-	height= images[0].getHeight();
-	return (new Dimension(width,height));
+	width=images[0].getWidth()/100;
+	height= images[0].getHeight()/100;
+	return (new Dimension(width*100,height*100));
     }
     
     @Override
@@ -76,43 +139,63 @@ class AquaPanel extends JPanel implements ActionListener{
 	for(int i=0; i<Fishes.size();i++){
 	    /* match the fish name with the suitable image */
 	    for(int l=1;l<8;l++)
-		    if (fishImages[l].equals(Fishes.get(i).name)){ /*TODO: accept names with _ */
-			Fishes.get(i).imageIndex=l;
-			break;
-		    }
+		if (fishImages[l].equals(Fishes.get(i).name)){ /*TODO: accept names with _ */
+		    Fishes.get(i).imageIndex=l;
+		    break;
+		}
 		    else /*image par défaut*/
 			Fishes.get(i).imageIndex=2;
-	    //System.out.println("fishImages[imageIndex]="+fishImages[Fishes.get(i).imageIndex]+";Fishes.get("+i+").name="+Fishes.get(i).name+";" );
+	    
 	    /*Actual Position*/
-	    x=Fishes.get(i).initPosition.get(0);
-	    y=Fishes.get(i).initPosition.get(1);
+	    x=width*Fishes.get(i).initPosition.get(0);
+	    // System.out.println("x= "+x);
+	    y=height*Fishes.get(i).initPosition.get(1);
 	    /*if the fish have an other position to move to*/
 	    if(Fishes.get(i).initPosition.size()>=4){
+		/* TODO: set intermediate positions & manage mobilityTime*/
+
 		/*destination*/
-		z=Fishes.get(i).initPosition.get(2);
-		w=Fishes.get(i).initPosition.get(3);
+		z=width*Fishes.get(i).initPosition.get(2);
+		w=height*Fishes.get(i).initPosition.get(3);
+		float TimeUnity=Fishes.get(i).mobilityTime/100;
+		float decX=(z-x)/TimeUnity;
+		float decY=(w-y)/TimeUnity;
+		System.out.println(decX+" ; "+decY);
 		/*Remove the privious position*/
 		Fishes.get(i).initPosition.remove(0);
 		Fishes.get(i).initPosition.remove(1);
 	    }
+	    else{
+		z=x;
+		w=y;
+	    }
+
 	    // System.out.println("Size= "+Fishes.get(i).initPosition.size());
 	    /*manage the proportional size*/
-	    scaleX=width*Fishes.get(i).dimensions[0]/100;
-	    scaleY=height*Fishes.get(i).dimensions[1]/100;
-	    /*display the fish*/
+	    scaleX=width*Fishes.get(i).dimensions[0];
+	    scaleY=height*Fishes.get(i).dimensions[1];
 	    g.drawImage(images[Fishes.get(i).imageIndex],x,y,scaleX,scaleY,this);
 	    
 	}
-	
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
-	/*x += 1;
-	  y += 1;*/
-	x=z;
-	y=w;
+	x =z;
+	y =w;
 	repaint();
     }
+    
+    
+    public String fishesToString(ArrayList<Fish> Fishes){
+	String res = "\n";
+	for (Fish fish: Fishes){
+	    res = res + "Fish " + fish.name +" at " +
+		fish.initPosition.get(0) + "x" + fish.initPosition.get(1)+ "," +
+		fish.dimensions[0] +"x"+ fish.dimensions[1] +" :  "+ ((fish.started == 1) ? "started" : "not started") +  "\n" ;
+	}
+	return res;
+    }
+    
 }
 
--- view/view3/Aquarium.java
+++ view/view3/Aquarium.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Aquarium {
-    private static  ArrayList<Fish> fishes;
     private static Pattern [] pattern;
     private static Matcher matcher;
     private static AquaPanel contentPane;
@@ -16,12 +15,35 @@ public class Aquarium {
     private static InetAddress address;
     private static ClientLog logger;
     private static JFrame frame ;
-    /*To be removed after establishing a connection with the server*/
     private static boolean connected=false;
-    /* create a scanner so we can read the command-line input*/
-    static Scanner	scanner = new Scanner(System.in);
-    private void displayGUI(String ImagesPath)
-    {
+    static Scanner scanner = new Scanner(System.in);
+
+    /***Ajouté de view4****/
+    private static Parser parser;
+    
+
+    private static void startFish(String name){
+	contentPane.setStartFish(name);
+    }
+    
+    private static void status(){
+	System.out.println(contentPane.fishesToString(contentPane.Fishes));
+    }
+
+    private static void addFish(String cmd){
+	String[] items =  parser.parseFishPosition(cmd);
+	contentPane.setAddFish(items);
+	
+    }
+    
+    public static void getFishes(String response){
+	String[][] items = parser.parseListFishPosition(response);
+	contentPane.setGetFishes(items);
+    }
+    
+    /***Fin Ajout view 4****/
+
+    private void displayGUI(String ImagesPath){
         frame = new JFrame("Aquarium");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         contentPane = new AquaPanel(ImagesPath);        
@@ -32,27 +54,8 @@ public class Aquarium {
 	frame.setVisible(true);
 	frame.setResizable(false);
     }
-    /*Méthode addFish provisoire pour pouvoir tester l'affichage*/
-   private static void addFish(String name)
-    {
-	System.out.println("calling addFish methode");
-	int[] dimensions=new int[2];
-	dimensions[0]=10;
-	dimensions[1]=10;
-	ArrayList<Integer> initPosition=new ArrayList<Integer>() ;
-	for(int j =0;j<44;j++){
-	    initPosition.add(2*j);
-	    initPosition.add(2*j);
-	    initPosition.add(2*j+1);
-	    initPosition.add(2*j+1);
-	}
-	Fish poiss=new Fish(name,initPosition,5,dimensions);
-	fishes.add(poiss);
-	contentPane.setFishes(fishes);
-    }
-
-    static private String promptIn()throws IOException
-    {
+    
+    static private String promptIn()throws IOException{
 	System.out.print(">"); 
 	/*get the input as a String*/
 	String cmd = scanner.nextLine();
@@ -68,116 +71,97 @@ public class Aquarium {
     }
     static private void promptOut(String response)throws IOException
     {	
-	System.out.print("<"+response); //+server's response
+	System.out.print("<"+response+"\n"); //+server's response
 	logger.info("Client received " + response);
 	
 	
     }
     
+    
     public static void main(String[] args) throws Exception
     {
-	fishes=new ArrayList<Fish>();
-	int i=0;
 	logger = new ClientLog();
-	// 	ClientLog logger = new ClientLog();
-	
 	/*Get Configuration data*/  
 	Config conf = new Config(args[0]);
 	port = conf.getTcpPort();
 	final String ImagesPath = conf.getVisualRepertory();
 	address = conf.getIpAddress();
 	try{
-	aquaCon = new AquaConnection(address, port);
+	aquaCon = new AquaConnection();
 	/*cmd patterns*/
-	pattern=new Pattern[10];  
-	
-	pattern[0]=Pattern.compile("OK");//,Pattern.CASE_INSENSITIVE);
+	pattern=new Pattern[11];  
+	pattern[0]=Pattern.compile("^OK");//,Pattern.CASE_INSENSITIVE);
 	//	pattern[1]= Pattern.compile("hello");
 	pattern[2]= Pattern.compile("greeting \\w+");
-	pattern[3]= Pattern.compile("addFish \\w+");
-	pattern[4]= Pattern.compile("delFish \\w+");
-	pattern[5]= Pattern.compile("startFish \\w+");
+	pattern[3]= Pattern.compile("^addFish ");
+	pattern[4]= Pattern.compile("^delFish ");
+	pattern[5]= Pattern.compile("^startFish \\w+");
 	pattern[6]= Pattern.compile("^log out");
 	pattern[7]= Pattern.compile("^bye");
 	pattern[8]= Pattern.compile("^getFishes");
 	pattern[9]= Pattern.compile("^getFishesContinuously");
+	pattern[10] = Pattern.compile("^status");
 	/*Prompt*/
-	System.out.print(">>>>>>>Enter your command please <<<<<<<\n");
+	//	System.out.print(">>>>>>>Enter your command please <<<<<<<\n");
 	String response ="",cmd="";
+	
 	while(cmd==""){  
+	    if (connected==false){
+		aquaCon.openConnection(address,port);
+		connected=true;
+	    }
 	    cmd=promptIn();
-	    if(i==0) i++;
-	    else
-		response=aquaCon.receive();
+	    response=aquaCon.receive();
 	    promptOut(response);
-	    System.out.println(String.format("\nYour command is %s ; Your response is %s",cmd, response));
+	    //	  System.out.println(String.format("\nYour command is %s ; Your response is %s",cmd, response));
 	    /*Handling response*/
-	    //        response ="greeting N1";//test
-	    if(pattern[2].matcher(response).matches())//;&&(pattern[1].matcher(cmd).find()))/*Hello & greeting*/
-		{
-		    if (connected==false){
+		if(pattern[2].matcher(response).matches())/* greeting*/
+		    {	
 			/*Display the aquarium*/
 			SwingUtilities.invokeLater(new Runnable(){    
 				@Override
 				public void run() {
-				   new Aquarium().displayGUI(ImagesPath);
+				    new Aquarium().displayGUI(ImagesPath);
 				}
 			    });
-			connected=true;
 		    }
+		else if (pattern[10].matcher(cmd).matches()){/*status*/
+		    status();
 		}
-	    else if(pattern[8].matcher(cmd).matches()){/*getFishes*/
-		System.out.println("calling setFish methode (to update the arrayList Fishes)");
+		else if(pattern[8].matcher(cmd).matches()){/*getFishes*/
+		    //		    System.out.println("calling setFish methode (to update the arrayList Fishes)");
+		    getFishes(response);
+		}
+		else if(pattern[9].matcher(cmd).matches()){/*getFishesContinuously*/
+		    //		    System.out.println("getFishesContinuously()");
+		    //		    getFishesContinuously();
 	    }
-	    else if(pattern[9].matcher(cmd).matches()){/*getFishesContinuously*/
-		System.out.println("listening continuously+ promptout() with each response");
-	    }
-	    else{response ="OK"; //test
-		if(pattern[0].matcher(response).matches()){/*OK*/
-		    if(pattern[3].matcher(cmd).matches())
-			{  /*addFish*/
-			    Pattern p = Pattern.compile(" ");
-			    // séparation en sous-chaînes
-			    String[] items = p.split(cmd);
-			    addFish(items[1]);
+		else{
+		    if(pattern[0].matcher(response).find()){/*OK*/
+		 /*test: addFish smilingFish at 61x52,4x3, RandomWayPoint*/
+			System.out.println("This is ok :D");
+			if(pattern[3].matcher(cmd).find()){  /*addFish*/
+			    addFish(cmd);
 			} 
-		    if(pattern[4].matcher(cmd).matches()) /*delFish*/
-			System.out.println("calling delFish methode"); ;
-		    if(pattern[5].matcher(cmd).matches()) /*startFish*/
-			System.out.println("calling startFish methode"); ;
-		}	
-		
-		//	response ="bye"; //test
-		if((pattern[6].matcher(cmd).matches())&&(pattern[7].matcher(response).find())){/*log out & bye*/
-		    if(frame.isDisplayable())
-			frame.setVisible(false);
-		    connected=false;
-		    
-		}
-		
+			if(pattern[4].matcher(cmd).find()) /*delFish*/
+			    System.out.println("calling delFish methode"); ;
+			if(pattern[5].matcher(cmd).matches()) /*startFish*/{
+			    startFish(cmd);
+			}
+		    }
+		    if((pattern[6].matcher(cmd).find())&&(pattern[7].matcher(response).find())){/*log out & bye*/
+			if(frame.isDisplayable())
+			    frame.setVisible(false);
+			aquaCon.closeConnection();
+			connected=false;	
+		    }	
 	    }
-	  
-	    /*  TO be Used in treating response
-	    // compilation de la regex
-	    Pattern p = Pattern.compile(":");
-	    // séparation en sous-chaînes
-	    String[] items = p.split("un:deux:trois");
-	    */
-		
-	    //		System.out.println(pattern[0].matcher(cmd).find());
-	    /*   try{
-		
-		 }
-		 catch(IOException e){
-		 System.out.println("Sorry.. IOError");	
-		 }	*/
-
-
 	    cmd="";
 	}
 		}catch(IOException e){
-	    System.out.println("No Server!!!!!!!!!!!!!!!!!!!!!!!!!!!");}
+	    System.out.println("No Server!!!");}
     }
     
     
 }
+
